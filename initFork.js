6f23be8717c0e1fee046cd55fd6879986efe2c0e
IceMetalPunk <IceMetalPunk@gmail.com>
2022-04-02 22:06:34 -0400

    Fix execSync command format

--- initFork.js
+++ initFork.js
@@ -4,6 +4,6 @@ import {execSync} from 'child_process'
 console.log('Setting up workspace...');
 fs.appendFileSync('.gitignore', '.env\nmkcert.exe');
 execSync('npm i');
-execSync('./mkcert.exe -install');
-execSync('./mkcert.exe localhost');
+execSync('mkcert.exe -install');
+execSync('mkcert.exe localhost');
 console.log('Done!');
